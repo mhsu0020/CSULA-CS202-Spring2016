74987cb31319ee66c0c52a36598cfbffd2ca3a95
Michael Hsu <pig0826usanj@gmail.com>
2016-04-14 19:22:29 -0700

    multiple astronauts

--- Week3/AstronautBookExample.java
+++ Week3/AstronautBookExample.java
@@ -6,6 +6,8 @@ public class AstronautBookExample {
 
 		//Astronaut has a books arrayList data field
 		Astronaut buzzAlderin = new Astronaut("buzzAlderin", 60);
+		Astronaut astro2 = new Astronaut("astro2", 70);
+		Astronaut astro3 = new Astronaut("astro3", 80);
 
 
 		//Creat Book objects
@@ -17,15 +19,28 @@ public class AstronautBookExample {
 		buzzAlderin.addBook(book);
 		buzzAlderin.addBook(book2);
 
-		//You can use the arrayList by doing buzzAlderin.favoriteBooks
 
-		ArrayList<Book> favoriteBooks = buzzAlderin.favoriteBooks;
-		int numberOfFavoriteBooks = favoriteBooks.size();
+		//Each astronaut has their own favoriteBooks arrayList
+		Book book3 = new Book("the shining", "stuff", "fantasy", "$1000", "1934-01-2", "AS12121212");
+		Book book4 = new Book("the stand", "magic", "fantasy", "$10000", "2004-02-2", "BD121212");
+		astro2.addBook(book3);
+		astro2.addBook(book4);
+
+		//You can use the arrayList by doing buzzAlderin.favoriteBooks
+		ArrayList<Book> buzzFavoriteBooks = buzzAlderin.favoriteBooks;
+		int numberOfFavoriteBooks = buzzFavoriteBooks.size();
 		System.out.println(numberOfFavoriteBooks);
 
-		//Printing out the books in the a
-		for (int i = 0; i < favoriteBooks.size(); i++) {
-			System.out.println(favoriteBooks.get(i));
+		//Printing out the books
+		for (int i = 0; i < buzzFavoriteBooks.size(); i++) {
+			System.out.println(buzzFavoriteBooks.get(i));
+		}
+
+		ArrayList<Book> astro2FavoriteBooks = astro2.favoriteBooks;
+
+		//Printing out the books
+		for (int i = 0; i < astro2.favoriteBooks.size(); i++) {
+			System.out.println(astro2.favoriteBooks.get(i));
 		}
 
 
